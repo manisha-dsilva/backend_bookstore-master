dbf2fe21d3e155f813001b775877b58c8d460aed
Dsilva Manisha Audrey <manisha.dsilva@al-enterprise.com>
2020-08-11 18:36:19 +0530

    pipeline

--- java-app/src/main/java/com/ngov/test/Application.java
+++ java-app/src/main/java/com/ngov/test/Application.java
@@ -3,21 +3,11 @@ package com.ngov.test;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-
-import com.fasterxml.jackson.databind.JsonDeserializer;
-
 
 @SpringBootApplication
 public class Application {
