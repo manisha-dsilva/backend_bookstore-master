046c138aa746d73d9f75c63bb321efc40f0dfdd2
Dsilva Manisha Audrey <manisha.dsilva@al-enterprise.com>
2020-08-12 18:35:54 +0530

    pipeline

--- java-app/src/main/java/com/ngov/test/Application.java
+++ java-app/src/main/java/com/ngov/test/Application.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 	
 	
